02c3ff149659f165ec550704fe32c359eeb2ed65
Yue Wang (MSR Student-Person Consulting) <v-yuewng@microsoft.com>
2017-06-27 16:55:26 +0800

    3

--- manual/cnn/cifar10/experiment1.py
+++ manual/cnn/cifar10/experiment1.py
@@ -97,7 +97,7 @@ for ii in range(1000000):
         temp_step += 1
         
 
-        if  temp_step >5 and  np.mean(train_vrloss[-5:]) -train_vrloss[-1]  < (model.lr/1000.0 ):
+        if  temp_step >5 and  np.mean(train_vrloss[-5:]) -train_vrloss[-1]  < (model.lr/1000.0 ) and model.lr > 1e-9:
             model.lr = model.lr / 10.0
             temp_step = 0
             print('learning rate decrease to ', model.lr)
