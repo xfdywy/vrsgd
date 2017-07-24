a0939badf5d27a6511a2f78b4d4cc193fb8302db
xfdywy <15118421@bjtu.edu.cn>
2017-07-24 17:58:38 +0900

    1

--- pytorch/cifar10_resnet/runexptesla01_resnet_6.sh
+++ pytorch/cifar10_resnet/runexptesla01_resnet_6.sh
@@ -1,9 +1,7 @@
-gpu=4
-CUDA_VISIBLE_DEVICES=${gpu} python  main_sqrt_other_traindata.py --lr 0.1 --variance 0 --entropy 0 --epoch 180 --net resnet20
-CUDA_VISIBLE_DEVICES=${gpu} python  main_sqrt_other_traindata.py  --lr 0.1 --variance 0.1 --entropy 0 --epoch 180 --net resnet20 
-CUDA_VISIBLE_DEVICES=${gpu} python  main_sqrt_other_traindata.py  --lr 0.1 --variance 0.05 --entropy 0 --epoch 180 --net resnet20 
-CUDA_VISIBLE_DEVICES=${gpu} python  main_sqrt_other_traindata.py  --lr 0.1 --variance 0.01 --entropy 0 --epoch 180 --net resnet20  
-CUDA_VISIBLE_DEVICES=${gpu} python  main_sqrt_other_traindata.py  --lr 0.1 --variance 0.005 --entropy 0 --epoch 180 --net resnet20 
-CUDA_VISIBLE_DEVICES=${gpu} python  main_sqrt_other_traindata.py  --lr 0.1 --variance 0.001 --entropy 0 --epoch 180 --net resnet20 
-CUDA_VISIBLE_DEVICES=${gpu} python  main_sqrt_other_traindata.py  --lr 0.1 --variance 0.5 --entropy 0 --epoch 180 --net resnet20
+cuda=4
+
+for index in 1 2 3 4 5 6
+do
+CUDA_VISIBLE_DEVICES=${cuda} python main_sqrt.py --lr 0.1 --variance 0.1 --entropy 1 --epoch 180 --net resnet20 --index ${index}
+done
 
