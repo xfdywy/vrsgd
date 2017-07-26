1167077ce6af7bfa76377d5431794215000cbae8
xfdywy <15118421@bjtu.edu.cn>
2017-07-26 11:32:33 +0900

    sh 1

--- pytorch/cifar10_resnet/runexptesla01_resnet_13.sh
+++ pytorch/cifar10_resnet/runexptesla01_resnet_13.sh
@@ -2,6 +2,6 @@ cuda=4
 
 for index in 1 2 3 4 5 6
 do
-CUDA_VISIBLE_DEVICES=${cuda} python main_mask_entropy_sqrt.py --lr 0.1 --variance 0.1 --entropy 0.1 --epoch 180 --net resnet20 --index ${index}
+CUDA_VISIBLE_DEVICES=${cuda} python main_sqrt_other_traindata_nowd.py --lr 0.1 --variance 0.1 --entropy 0.1 --epoch 180 --net resnet20 --index ${index}
 done
  
